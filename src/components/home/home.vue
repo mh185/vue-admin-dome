bc6efdf71a245afc149d0ac5658c0c4dca272e12
mh185 <2214847075@qq.com>
2020-08-04 16:48:38 +0800

    头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>home</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="8">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="22" class="middle">
+          <div class="grid-content bg-purple-light">
+            <h3>电商后台管理系统</h3>
+          </div>
+        </el-col>
+        <el-col :span="2" class="loginout">
+          <div class="grid-content bg-purple">
+            <a href>退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -7,4 +29,23 @@ export default {};
 </script>
 
 <style>
+.container {
+  height: 100%;
+}
+.header {
+  background-color: #b3c0d1;
+}
+.aside {
+  background-color: #d3dce6;
+}
+.main {
+  background-color: #e9eef3;
+}
+.middle {
+  text-align: center;
+}
+.loginout {
+  line-height: 60px;
+  text-decoration: none;
+}
 </style>
